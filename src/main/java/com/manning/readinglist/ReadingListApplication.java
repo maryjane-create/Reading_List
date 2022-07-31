bcd6eea6420c4d89b34082b4a9f5b4bb74b79ae2
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-07-31 17:14:14 +0100

    second commit

---
+++ src/main/java/com/manning/readinglist/ReadingListApplication.java
@@ -0,0 +1,13 @@
+package com.manning.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListApplication {
+
+    public static void main(String[] args) throws  Throwable {
+        SpringApplication.run(ReadingListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manning/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,19 @@
+package com.manning.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+
+
+@WebAppConfiguration
+class ReadingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
